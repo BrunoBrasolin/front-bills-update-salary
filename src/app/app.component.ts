67d8a0056f3339d51711cc179a9ff76e7e64ffe8
Bruno Brasolin <brunobrasolinc@gmail.com>
2024-02-28 22:29:00 -0300

    Style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,23 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppService } from './app.service';
 import { ApiDtoInterface, PersonInterface } from './app.model';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -26,7 +38,7 @@ export class AppComponent {
       salary: this.salary,
       person: this.person
     }
-    
+
     this.service.UpdateSalary(dto).subscribe(s => console.log(s));
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()), provideAnimationsAsync()
   ]
 };
